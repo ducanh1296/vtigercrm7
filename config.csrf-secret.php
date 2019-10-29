e809ae96df399b3a6949db6e6dcaed5c20b32254
ducanh1296 <leducanh10296@gmail.com>
2019-10-29 14:07:13 +0700

    vtigercrm7

---
+++ config.csrf-secret.php
@@ -0,0 +1,1 @@
+<?php $secret = "b9c39583dca4b1d90b2f6ba441714eb844d9be10";
---
+++ test/templates_c/v7/ede5748653315de512de66eb63ccd0edc8f1cf49.file.IndexPostProcess.tpl.php
@@ -0,0 +1,28 @@
+<?php /* Smarty version Smarty-3.1.7, created on 2019-10-29 07:06:15
+         compiled from "C:\xampp\htdocs\vtigercrm\includes\runtime/../../layouts/v7\modules\Vtiger\IndexPostProcess.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:10948902455db7e4e7c1a776-64305116%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'ede5748653315de512de66eb63ccd0edc8f1cf49' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\vtigercrm\\includes\\runtime/../../layouts/v7\\modules\\Vtiger\\IndexPostProcess.tpl',
+      1 => 1520586669,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '10948902455db7e4e7c1a776-64305116',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.7',
+  'unifunc' => 'content_5db7e4e7c1b88',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5db7e4e7c1b88')) {function content_5db7e4e7c1b88($_smarty_tpl) {?>
+
+
+
+	</div>
+</div>
+</div><?php }} ?>
